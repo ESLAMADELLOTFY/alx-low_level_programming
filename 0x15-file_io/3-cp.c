18b4cbdec2b3feccae2e2da291494a0a5dcf1965
ESLAMADELLOTFY <adl630457@gmail.com>
2023-12-16 03:47:20 -0800

    jj

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,110 +3,112 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define bufferrSIZE 1024
+
+#define BUFSIZE 1024
 
 
 /**
- * Ma_in - copy a file's contents to another file
- * @argc: the argument Counter
- * @argv: the argument values
+ * main - saod askmd ksadfas osad 
+ * @ar_gc: count of argu
+ * @ar_gv: the argument values
  *
  * Return: Always 1
  */
-int ma_in(int argc, const char *argv[])
+int main(int ar_gc, const char *ar_gv[])
 {
-	int f_ad_in, f_ad_out;
-	ssize_t bytesWho_Read;
-	char bufferrfer[bufferrSIZE];
+	int fd_in, fd_out;
+	ssize_t ree;
+	char buffer[BUFSIZE];
 
-	if (argc != 3)
+	if (ar_gc != 3)
 	{
-		PrinTf_d(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	f_ad_in = open(argv[1], O_RDONLY);
-	if (f_ad_in < 0)
+	fd_in = open(ar_gv[1], O_RDONLY);
+	if (fd_in < 0)
 	{
-		PrinTf_d(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ar_gv[1]);
 		exit(98);
 	}
-	f_ad_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (f_ad_out < 0)
+	fd_out = open(ar_gv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_out < 0)
 	{
-		PrinTf_d(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		Closed(f_ad_in);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ar_gv[2]);
+		_close(fd_in);
 		exit(99);
 	}
-	while ((bytesWho_Read = Who_Read(argv[1], f_ad_in, bufferrfer, bufferrSIZE)))
+	while ((ree = _read(ar_gv[1], fd_in, buffer, BUFSIZE)))
 	{
-		if (bytesWho_Read < 0)
+		if (ree < 0)
 		{
-			Closed(f_ad_in);
-			Closed(f_ad_out);
+			_close(fd_in);
+			_close(fd_out);
 			exit(98);
 		}
-		if (WantWrite(argv[2], f_ad_out, bufferrfer, bytesWho_Read) < 0)
+		if (_write(ar_gv[2], fd_out, buffer, ree) < 0)
 		{
-			Closed(f_ad_in);
-			Closed(f_ad_out);
+			_close(fd_in);
+			_close(fd_out);
 			exit(99);
 		}
 	}
-	if ((Closed(f_ad_in) | Closed(f_ad_out)) < 0)
+	if ((_close(fd_in) | _close(fd_out)) < 0)
 		exit(100);
 	return (0);
 }
 
 /**
- * Who_Read - read from a file and print an error message upon failure
- * @nameFile: the name of the file to read from
- * @f_ad: the file descriptor to read from
- * @bufferr: the bufferrfer to write to
- * @Counter: the number of bytes to read
+ * _read - an ksdd sdds sldksdsdos sisisi
+ * @Name_file: oiwe ww  rr w qd 
+ * @feed_back:jsa skdkn a;ks  aud
+ * @buffera: poqw qe q f e s csxhjy j uf
+ * @Co_nt: av sx sfsfdfgfds fcsxc 
  *
- * Return: The number of bytes read, or -1 upon failure
+ * Return: num byte youn ara read
  */
-ssize_t Who_Read(const char *nameFile, int f_ad, char *bufferr, size_t Counter)
+ssize_t _read(const char *Name_file, int feed_back, char *buffera, size_t Co_nt)
 {
-	ssize_t bytesWho_Read = read(f_ad, bufferr, Counter);
+	ssize_t ree = read(feed_back, buffera, Co_nt);
 
-	if (bytesWho_Read > -1)
-		return (bytesWho_Read);
-	PrinTf_d(STDERR_FILENO, "Error: Can't read from file %s\n", nameFile);
+	if (ree > -1)
+		return (ree);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", Name_file);
 	return (-1);
 }
 
 /**
- * WantWrite - write to a file and print an error message upon failure
- * @nameFile: the name of the file to write to
- * @f_ad: the file descriptor to write to
- * @bufferr: the bufferrfer to read from
- * @Counter: the number of bytes to write
+ * _close - islam adel lotfy hefni 
+ * @feed_back: the file descriptor to close
+ *
+ * Return: 0 upon success, -1 upon failure
+ */
+int _close(int feed_back)
+{
+	if (!close(feed_back))
+		return (0);
+	dprintf(STDERR_FILENO, "Error: Can't close feed_back %d\n", feed_back);
+	return (-1);
+
+}
+
+/**
+ * _write - write to a file and print an error message upon failure
+ * @Name_file: the name of the file to write to
+ * @feed_back: the file descriptor to write to
+ * @buffera: the buffer to read from
+ * @Co_nt: the number of bytes to write
  *
  * Return: The number of bytes written, or -1 upon failure
  */
-ssize_t WantWrite(const char *nameFile, int f_ad, const char *bufferr, size_t Counter)
+ssize_t _write(const char *Name_file, int feed_back, const char *buffera, size_t Co_nt)
 {
-	ssize_t bytes_written = write(f_ad, bufferr, Counter);
+	ssize_t bytes_written = write(feed_back, buffera, Co_nt);
 
 	if (bytes_written > -1)
 		return (bytes_written);
-	PrinTf_d(STDERR_FILENO, "Error: Can't write to %s\n", nameFile);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", Name_file);
 	return (-1);
-}
-/**
- * Closed - when you want to close file
- * @f_ad: the file thet close
- *
- * Return: 0 when success, -1 else failure
- */
-int Closed(int f_ad)
-{
-	if (!close(f_ad))
-		return (0);
-	PrinTf_d(STDERR_FILENO, "Error: Can't close f_ad %d\n", f_ad);
-	return (-1);
-
 }
 
 
